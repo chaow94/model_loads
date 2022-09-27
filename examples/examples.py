b9dd2cb4039262d1918b39950b8de39b0f230427
cc <dwc1307302@gmail.com>
2022-09-27 20:22:17 +0800

    Update examples.py

--- examples/examples.py
+++ examples/examples.py
@@ -3,9 +3,11 @@ import torch.cuda
 import model_loads as lo
 import torchvision.models as models
 
-model = models.MobileNetV2()
+model = models.MobileNetV2().cuda()
+
 model_path = "../examples/models/pth/mobilenet_v2-b0353104.pth"
-lo.load_models(model_path, model, use_gpu=True)
+lo.load_models(model_path, model)
+
 print(model)
 print(type(model))
 
@@ -18,6 +20,6 @@ print("use cuda:", torch.cuda.is_available())
 
 model = models.MobileNetV2()
 model_path = "models/pth/mobilenet_v2-b0353104.pth"
-lo.load_models(model_path, model, use_gpu=True)
+lo.load_models(model_path, model)
 print(model)
 print(type(model))
